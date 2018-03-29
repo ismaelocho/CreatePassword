9c7a34e30045aea8ec20b0334480bfc73d1d1f4e
Ismael Ochoa <ismaelocho@gmail.com>
2018-03-29 15:27:56 -0700

    Create generatePassword.php

---
+++ generatePassword.php
@@ -0,0 +1,17 @@
+<?php 
+function generatePassword() {
+  //Initialize the random password
+  $password = '';
+
+  //Initialize a random desired length
+  $desired_length = rand(8, 12);
+
+  for($length = 0; $length < $desired_length; $length++) {
+    //Append a random ASCII character (including symbols)
+    $password .= chr(rand(32, 126));
+  }
+
+  return $password;
+}
+
+?>
